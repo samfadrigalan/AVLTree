077f95d5c307795d9bf3463eb2e7fe7b3af8c28a
Samantha Fadrigalan <snmfadrigalan@gmail.com>
2014-10-30 22:46:13 -0500

    Added PrintWriter

--- src/avltreeapp/AVLTreeApp.java
+++ src/avltreeapp/AVLTreeApp.java
@@ -13,6 +13,7 @@ public class AVLTreeApp {
         try{
             AVLTree<Integer> tree = new AVLTree<>();
             Scanner fin = new Scanner(new File("AVLtree-input-2.txt"));
+            PrintWriter fout = new PrintWriter(new File("AVLOutput.txt"));
             while(fin.hasNext()){
                 String operation = fin.next();
                 boolean found = false;
@@ -30,19 +31,22 @@ public class AVLTreeApp {
                 else if(operation.compareTo("SC") == 0){//Successor
                     successor = tree.successor(tree.root(), fin.nextInt());
                     System.out.println(successor.key());
+                    fout.print(successor.key());
                 }
                 else if(operation.compareTo("SE") == 0){//Select
                     keyToSearch = fin.nextInt();
                     selected = tree.select(tree.root(), keyToSearch);
                     System.out.println(selected.key());
+                    fout.println(selected.key());
                 }
                 else{//Rank
                     keyToSearch = fin.nextInt();
                     rank = tree.rank(tree.root(), keyToSearch);
                     System.out.println(rank);
+                    fout.println(rank);
                 }
             }
-            
+            fout.close();
             fin.close();
         }
         catch(IOException e){
